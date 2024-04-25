94736d133511765ee9cc5620e4815fe08507b476
Hrithik_Anagire <hrithikanagire@gmail.com>
2024-04-26 01:41:00 +0530

    env file added

---
+++ env.config.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+
+export default {
+  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+import config from "./env.config";
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: config.NEXT_PUBLIC_BASE_URL,
 };
 
 export default nextConfig;
