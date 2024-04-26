d9ae489091485771f95cd519cd0b4ee02da18ea5
Hrithik_Anagire <hrithikanagire@gmail.com>
2024-04-26 11:54:34 +0530

    feature: deploy try7

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_URL || "",
+  basePath: "/material-collection-app",
 };
 
 export default nextConfig;
